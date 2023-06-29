c4a13c33999c623ffd48cf8b5480ba0f1a6933fa
mainul35 <mainuls18@gmail.com>
2023-06-29 20:42:54 +0700

    Spring boot oauth2 Authorization Server

---
+++ authorization-server/src/test/java/com/mainul35/authorizationserver/AuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mainul35.authorizationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
