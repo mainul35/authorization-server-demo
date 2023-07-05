276f4fbc6120e0457f002017e810aa29477ea8e2
mainul35 <mainuls18@gmail.com>
2023-07-06 01:26:09 +0700

    committing configurations

--- social-login-client/src/main/java/com/mainul35/socialloginclient/SecurityConfig.java
+++ social-login-client/src/main/java/com/mainul35/socialloginclient/SecurityConfig.java
@@ -1,25 +1,25 @@
-package com.mainul35.socialloginclient;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig {
-
-    @Bean
-    SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
-        return http
-                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> {
-                    authorizationManagerRequestMatcherRegistry.requestMatchers("/").permitAll()
-                            .anyRequest().authenticated();
-                })
-                .oauth2Login(Customizer.withDefaults())
-                .formLogin(Customizer.withDefaults())
-                .build();
-    }
-}
+//package com.mainul35.socialloginclient;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig {
+//
+//    @Bean
+//    SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
+//        return http
+//                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> {
+//                    authorizationManagerRequestMatcherRegistry.requestMatchers("/").permitAll()
+//                            .anyRequest().authenticated();
+//                })
+//                .oauth2Login(Customizer.withDefaults())
+//                .formLogin(Customizer.withDefaults())
+//                .build();
+//    }
+//}
