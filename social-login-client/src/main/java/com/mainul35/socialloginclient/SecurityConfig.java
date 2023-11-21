ae3d44e6a9dd8a00adaeed9f7092ede1a8492b78
mainul35 <mainuls18@gmail.com>
2023-11-21 12:36:11 +0700

    changes for PKCE in progress

--- social-login-client/src/main/java/com/mainul35/socialloginclient/SecurityConfig.java
+++ social-login-client/src/main/java/com/mainul35/socialloginclient/SecurityConfig.java
@@ -5,6 +5,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizationRequestResolver;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestCustomizers;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestRedirectFilter;
+import org.springframework.security.oauth2.client.web.server.DefaultServerOAuth2AuthorizationRequestResolver;
+import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizationRequestResolver;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,15 +19,31 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
+    SecurityFilterChain securityFilterChain (
+            HttpSecurity http,
+            ClientRegistrationRepository clientRegistrationRepository // To enable PKCE authentication
+    ) throws Exception {
+
+        String baseUrl = OAuth2AuthorizationRequestRedirectFilter.DEFAULT_AUTHORIZATION_REQUEST_BASE_URI; //"/oauth2/authorize";
+        DefaultOAuth2AuthorizationRequestResolver resolver
+                = new DefaultOAuth2AuthorizationRequestResolver(clientRegistrationRepository, baseUrl);
+        resolver.setAuthorizationRequestCustomizer(OAuth2AuthorizationRequestCustomizers.withPkce());
+
         return http
+                .headers(httpSecurityHeadersConfigurer ->
+                        httpSecurityHeadersConfigurer
+                                .httpStrictTransportSecurity(Customizer.withDefaults())
+                )
                 .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> {
                     authorizationManagerRequestMatcherRegistry
                             .requestMatchers("/").permitAll()
                             .anyRequest().authenticated();
                 })
-                .oauth2Login(Customizer.withDefaults())
-                .formLogin(Customizer.withDefaults())
+                .oauth2Login(oAuth2LoginConfigurer -> {
+                    oAuth2LoginConfigurer.loginPage("/oauth2/authorization/oidc-client");
+                    oAuth2LoginConfigurer.authorizationEndpoint(authorizationEndpointConfig -> authorizationEndpointConfig.authorizationRequestResolver(resolver));
+                })
+                .oauth2Client(Customizer.withDefaults())
                 .build();
     }
 }
